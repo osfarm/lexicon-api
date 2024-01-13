522092aaed9dcdd39db70df8450aa5ecf27c8e56
David JOULIN <djoulin@ekylibre.com>
2024-01-13 19:43:26 +0100

    Test

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
 # Elysia with Bun runtime
 
-## Getting Started
-To get started with this template, simply paste this command into your terminal:
+## Configuration
+
+Install [NVM](https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating)
+
+Install NodeJS with NVM
+
 ```bash
-bun create elysia ./elysia-example
+nvm install
+nvm use
+```
+
+Install Bun
+
+```bash
+curl https://bun.sh/install | bash
+```
+
+Install dependencies
+
+```bash
+bun install
+```
+
+Create a .env.local file with correct variables for your Postgresql database.
+
+```env
+LEXICON_DB_USER=user
+LEXICON_DB_PWD=password
+LEXICON_DB_HOST=localhost
+LEXICON_DB_PORT=5432
+LEXICON_DB_DATABASE=my_db
+LEXICON_DB_SCHEMA=my_schema
 ```
 
 ## Development
+
 To start the development server run:
 ```bash
 bun run dev
 ```
 
-Open http://localhost:3000/ with your browser to see the result.
+Open http://localhost:3003/ with your browser to see the result.
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Elysia } from "elysia";
 import { Client } from "pg"
 
-const { LEXICON_DB_USER, LEXICON_DB_PWD, LEXICON_DB_HOST, LEXICON_DB_DATABASE } = Bun.env
+const { LEXICON_DB_USER, LEXICON_DB_PWD, LEXICON_DB_HOST, LEXICON_DB_DATABASE, LEXICON_DB_PORT, LEXICON_DB_SCHEMA } = Bun.env
 
-const dbUrl = `postgresql://${LEXICON_DB_USER}:${LEXICON_DB_PWD}@${LEXICON_DB_HOST}:26257/${LEXICON_DB_DATABASE}?sslmode=verify-full`
+const dbUrl = `postgresql://${LEXICON_DB_USER}:${LEXICON_DB_PWD}@${LEXICON_DB_HOST}:${LEXICON_DB_PORT}/${LEXICON_DB_DATABASE}`
 
 const client = new Client(dbUrl);
 
@@ -19,7 +19,7 @@ console.log(
 const getRecord = async (collection: string, id: string): Promise<any> => {
     await client.connect();
     try {
-      const results = await client.query(`SELECT * FROM public.master_${collection} WHERE reference_name='${id}'`);
+      const results = await client.query(`SELECT * FROM ${LEXICON_DB_SCHEMA}.${collection} WHERE reference_name='${id}'`);
       if (results.rows.length === 0) {
         return {}
       }
@@ -34,7 +34,7 @@ const getRecord = async (collection: string, id: string): Promise<any> => {
 const getRecords = async (collection: string): Promise<any> => {
   await client.connect();
   try {
-    const results = await client.query(`SELECT * FROM public.master_${collection} LIMIT 10`);
+    const results = await client.query(`SELECT * FROM ${LEXICON_DB_SCHEMA}.${collection} LIMIT 10`);
     if (results.rows.length === 0) {
       return {}
     }
