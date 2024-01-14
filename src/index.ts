5ec0571e46a2bec3190817133bff76e5dfcca57e
Olivier Lépine <olivier@lepine.fr>
2024-01-14 18:24:24 +0100

    Add ECS action

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Elysia } from "elysia";
 import { Client } from "pg"
 
-const { LEXICON_DB_USER, LEXICON_DB_PWD, LEXICON_DB_HOST, LEXICON_DB_DATABASE } = Bun.env
+const { LEXICON_DB_USER, LEXICON_DB_PWD, LEXICON_DB_HOST, LEXICON_DB_PORT, LEXICON_DB_DATABASE } = Bun.env
 
-const dbUrl = `postgresql://${LEXICON_DB_USER}:${LEXICON_DB_PWD}@${LEXICON_DB_HOST}:26257/${LEXICON_DB_DATABASE}?sslmode=verify-full`
+const dbUrl = `postgresql://${LEXICON_DB_USER}:${LEXICON_DB_PWD}@${LEXICON_DB_HOST}:${LEXICON_DB_PORT}/${LEXICON_DB_DATABASE}?sslmode=verify-full`
 
 const client = new Client(dbUrl);
 
